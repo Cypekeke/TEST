faa850609b845f5f91058da0f4429af98b2d7905
test <cyprian.mill@gmail.com>
2024-10-12 12:24:34 +0200

    first "hello world"

---
+++ ArithmeticOperations.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperations {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 6;
+            int c= a + b;
+            int d= a - b;
+            int e= a * b;
+
+            System.out.println( a + " + " + b + " = " + c);
+            System.out.println( a + " - " + b + " = " + d);
+            System.out.println( a + " * " + b + " - " + e);
+
+            System.out.println(a);
+            System.out.println(a++);
+            System.out.println(a);
+            System.out.println(++a);
+
+        }
+    }
+
